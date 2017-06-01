ac5f9b3bd66763112b5ed61bc0f117e4407eac37
Jake Funke <jifunks@gmail.com>
2017-05-31 18:06:07 -0700

    Update dynamic task view

--- tasklist/views.py
+++ tasklist/views.py
@@ -2,6 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Task
 
 def task_list(request):
-    return render(request, 'tasklist/task_list.html', {})
+    # filter so that we only see tasks created prior to now, order by date created
+    tasks = Task.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'tasklist/task_list.html', {'tasks': tasks})
