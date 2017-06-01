68bc4095a3226a3d3ec23851786982eb2c327c2e
Jake Funke <jifunks@gmail.com>
2017-06-01 16:10:41 -0700

    Can add new tasks to task list

--- tasklist/views.py
+++ tasklist/views.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Task
-
+from .forms import TaskForm
 def task_list(request):
     # filter so that we only see tasks created prior to now, order by date created
-    tasks = Task.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
-    return render(request, 'tasklist/task_list.html', {'tasks': tasks})
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.owner = request.user
+            task.published_date = timezone.now()
+            task.save()
+            tasks = Task.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+            return render(request, 'tasklist/task_list.html', {'tasks': tasks, 'form':TaskForm})
+        else:
+            tasks = Task.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+            # TODO: handle form text here maybe?
+            return render(request, 'tasklist/task_list.html', {'tasks': tasks, 'form':TaskForm})
