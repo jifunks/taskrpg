7358341396b96591114d31c5967b5c21ba19a528
Jake Funke <jifunks@gmail.com>
2017-07-10 19:49:14 -0700

    Stage changes for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn timerpg.wsgi --log-file -
--- timerpg/settings.py
+++ timerpg/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '9%*ddrvxekxqd92^@hlyo7tn!jjpe#%%!frp1je$i0$w#okefq'
 DEBUG = True
 
 # TODO: add real host name here once deployed!
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
 # Application definition
@@ -82,6 +82,11 @@ DATABASES = {
     }
 }
 
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500) # instead of db_from_env = dj_database_url.config(), improves performance
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -120,5 +125,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# Old (pre-heroku)
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
